45a2c3d9379f2d1ce373b66755ae593efcca319a
gianniotisk <kgianniotis@york.citycollege.eu>
2022-05-12 21:34:54 +0300

    Updated

--- Armies.java
+++ Armies.java
@@ -2,4 +2,26 @@
 public class Armies {
 	private boolean deployed;
 	private int armyNr;
+	
+	
+	
+	public Armies(boolean deployed, int armyNr) {
+		super();
+		this.deployed = deployed;
+		this.armyNr = armyNr;
+	}
+	public boolean isDeployed() {
+		return deployed;
+	}
+	public void setDeployed(boolean deployed) {
+		this.deployed = deployed;
+	}
+	public int getArmyNr() {
+		return armyNr;
+	}
+	public void setArmyNr(int armyNr) {
+		this.armyNr = armyNr;
+	}
+	
+	
 }
